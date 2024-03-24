9c0927a54afcd0bda49c9fecb1a4271555e59339
fabianhaef <fabian.haefi@gmail.com>
2024-03-24 22:01:25 +0100

    gallrey fix on mobile,

--- frontend/src/js/features/scrollSlider.js
+++ frontend/src/js/features/scrollSlider.js
@@ -3,71 +3,74 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 import { debounce } from "./helpers";
 
 const setupScrollSliders = () => {
-  const scrollSliders = document.querySelectorAll('.imagescroll');
-  const allImages = document.querySelectorAll('.scrollimage-image');
+    const scrollSliders = document.querySelectorAll('.imagescroll');
+    const allImages = document.querySelectorAll('.scrollimage-image');
 
-  if (!scrollSliders.length) return;
+    if (!scrollSliders.length) return;
 
-  gsap.registerPlugin(ScrollTrigger);
+    gsap.registerPlugin(ScrollTrigger);
 
-  const calculateContainerWidth = (filteredImages, wrapper) => {
-    // Calculating imagescroll container width
-    let totalWidth = 0;
+    const calculateContainerWidth = (filteredImages, wrapper) => {
+        // Calculating imagescroll container width
+        let totalWidth = 0;
 
-    filteredImages.forEach((i, index) => {
-      const width = i.getBoundingClientRect().width;
-      if (index + 1 !== filteredImages.length) {
-        totalWidth += width + 30;
-      }
-    });
+        filteredImages.forEach((i, index) => {
+            const width = i.getBoundingClientRect().width;
+            if (index + 1 !== filteredImages.length) {
+                totalWidth += width + 20;
+            }
+        });
 
-    if (window.innerWidth < 768) {
-      totalWidth -= 50;
+        if (window.innerWidth < 768) {
+            totalWidth -= 50;
+        }
+
+
+        if (window.innerWidth < 768) {
+            totalWidth = totalWidth - 20;
+        } else {
+            totalWidth = totalWidth - 50;
+        }
+
+        wrapper.style.width = `${totalWidth}px`;
+        wrapper.style.maxWidth = `${totalWidth}px`;
     }
 
-    wrapper.style.width = `${totalWidth}px`;
-    wrapper.style.maxWidth = `${totalWidth}px`;
-  }
 
+    scrollSliders.forEach(img => {
+        const wrapper = img.querySelector('.imagescroll__wrapper');
+        const inner = img.querySelector('.imagescroll__inner');
 
-  scrollSliders.forEach(img => {
-    const wrapper = img.querySelector('.imagescroll__wrapper');
-    const content = img.querySelector('.imagescroll__content');
-    const inner = img.querySelector('.imagescroll__inner');
+        // Some responsive stuff for both text & scroll container
+        window.addEventListener('resize', () => {
+            calculateContainerWidth(allImages, wrapper);
+        });
 
-    // Some responsive stuff for both text & scroll container
-    window.addEventListener('resize', () => {
-      if (content) {
-        content.style.width = document.body.scrollWidth - 30 + 'px';
-      }
-      calculateContainerWidth(allImages, wrapper);
-    });
-
-    // Some init stuff
-    calculateContainerWidth(allImages, wrapper);
-
-    ScrollTrigger
-      .create({
-        trigger: img,
-        start: 'center center',
-        end: '+=100%',
-        pin: true,
-        pinSpacing: false,
-        onUpdate: self => {
-          const progress = self.progress;
-          const percent = Math.round(progress * 100);
-          inner.style.transform = `translateX(-${percent}%)`;
-        }
-      });
-
-      const updateOnReveal = debounce(() => {
-        ScrollTrigger.refresh();
+        // Some init stuff
         calculateContainerWidth(allImages, wrapper);
-      }, 250);
 
-      // Refresh Scrolltrigger when lazyloaded images are in the dom
-      window.addEventListener('lazyloaded', () => updateOnReveal());
-  });
+        ScrollTrigger
+            .create({
+                trigger: img,
+                start: 'center center',
+                end: '+=100%',
+                pin: true,
+                pinSpacing: false,
+                onUpdate: self => {
+                    const progress = self.progress;
+                    const percent = Math.round(progress * 100);
+                    inner.style.transform = `translateX(-${percent}%)`;
+                }
+            });
+
+        const updateOnReveal = debounce(() => {
+            ScrollTrigger.refresh();
+            calculateContainerWidth(allImages, wrapper);
+        }, 250);
+
+        // Refresh Scrolltrigger when lazyloaded images are in the dom
+        window.addEventListener('lazyloaded', () => updateOnReveal());
+    });
 }
 
 
