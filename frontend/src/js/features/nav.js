a77c800e66b4ad45369abddae3f83b0533a6be6b
fabianhaef <fabian.haefi@gmail.com>
2024-03-29 19:12:55 +0100

    nav bugfixes, pollyfill

--- frontend/src/js/features/nav.js
+++ frontend/src/js/features/nav.js
@@ -3,6 +3,7 @@ const burger = document.querySelector('.header__burger')
 const navWrapper = document.querySelector('.nav__wrapper')
 const nav = document.getElementById('nav')
 const header = document.getElementById('header')
+const navLinks = document.querySelectorAll('.nav__link.mobile');
 
 function initNav() {
     navTrigger.addEventListener('click', () => {
@@ -23,3 +24,11 @@ function toggleBurger() {
 if (navTrigger && nav) {
     initNav()
 }
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('nav--active');
+        header.classList.remove('nav--active');    
+        burger.classList.remove('active');
+    })
+})
